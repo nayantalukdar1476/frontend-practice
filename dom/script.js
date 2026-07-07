9e0765b99f2d20adb1f33d1f8a3978f12e2290c4
nayan talukdar <nayantalukdar1476@gmail.com>
2026-07-07 16:41:30 +0530

    download card

--- dom/script.js
+++ dom/script.js
@@ -1,28 +1,27 @@
-let main = document.querySelector("main");
 let btn = document.querySelector("button");
+let h2 = document.querySelector("h2");
+let inner = document.querySelector(".inner");
+let h4 = document.querySelector("h4");
+let span = document.querySelector("h4 span");
+let a = 0;
 
-let arr = ['nayan', 'talukdar', 'vikas', 'shukla', 'chinu', 'hey guys', 'im fine what about you buddy','im from indore ', 'learning js from scratch', 'nonnu is very understanding'];
+btn.addEventListener("click", function () {
+  btn.style.pointerEvents = "none";
 
-btn.addEventListener("click" , function(){
-  let h1 = document.createElement("h1");
-  let x = Math.random()*100;
-  let y = Math.random()*100;
-  let rot = Math.random()*360;
-  let scl = Math.random()*2;
-  
-  let c1 = Math.floor(Math.random()*256);
-  let c2 = Math.floor(Math.random()*256);
-  let c3 = Math.floor(Math.random()*256);
+  let num = 50 + Math.floor(Math.random()*100);
 
-  let a = Math.floor(Math.random()*arr.length);
+  let int = setInterval(function () {
+    a++;
+    h2.innerHTML = a + "%";
+    inner.style.width = a + "%";
+    h4.style.scale = 1;
+    span.innerHTML = num/10;
+  }, num);
 
-  h1.innerHTML = arr[a];
-  h1.style.position = 'absolute';
-  h1.style.top = x+'%';
-  h1.style.left = y+'%';
-  h1.style.rotate = rot + 'deg';
-  h1.style.scale = scl;
-  h1.style.color = `rgb(${c1},${c2},${c3})`;
-
-  main.appendChild(h1)
-})
+  setTimeout(function () {
+    clearInterval(int);
+    btn.style.opacity = 0.5;
+    btn.innerHTML = "Downloaded";
+    h4.innerHTML = '<a href="#">Open File</a>';
+  }, num*100);
+});
