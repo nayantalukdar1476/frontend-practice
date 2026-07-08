56fa132e09bbe5259a896d1ec7d955c1d8e07325
nayan talukdar <nayantalukdar1476@gmail.com>
2026-07-08 23:07:44 +0530

    insta like

--- dom-practice/script.js
+++ dom-practice/script.js
@@ -1,27 +1,21 @@
-let btn = document.querySelector("button");
-let h2 = document.querySelector("h2");
-let inner = document.querySelector(".inner");
-let h4 = document.querySelector("h4");
-let span = document.querySelector("h4 span");
-let a = 0;
+let img = document.querySelector("img");
+let i = document.querySelector("i");
 
-btn.addEventListener("click", function () {
-  btn.style.pointerEvents = "none";
-
-  let num = 50 + Math.floor(Math.random()*100);
-
-  let int = setInterval(function () {
-    a++;
-    h2.innerHTML = a + "%";
-    inner.style.width = a + "%";
-    h4.style.scale = 1;
-    span.innerHTML = num/10;
-  }, num);
-
-  setTimeout(function () {
-    clearInterval(int);
-    btn.style.opacity = 0.5;
-    btn.innerHTML = "Downloaded";
-    h4.innerHTML = '<a href="#">Open File</a>';
-  }, num*100);
+img.addEventListener("dblclick", function () {
+  i.style.opacity = 1;
+  i.style.transform = "translate(-50%, -50%) scale(1) rotate(0deg)";
+  
+  setTimeout(function(){
+      i.style.transform = "translate(-50%, -400%) scale(1) rotate(50deg)";
+    }, 600);
+    
+    setTimeout(function(){
+      i.style.opacity = 0;
+  }, 800);
+    
+    
+    setTimeout(function(){
+      i.style.transform = "translate(-50%, -50%) scale(1) rotate(-50deg)";
+  }, 1000);
+    
 });
